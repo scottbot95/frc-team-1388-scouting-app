8bf35e79c4cf7ba205c34631d98c55381399e4d3
Javaking <javaking.clanteam@gmail.com>
2013-01-15 22:47:22 -0800

    Converted to SherlockActionBar

--- src/com/javaking/clanteam/scouting/MainActivity.java
+++ src/com/javaking/clanteam/scouting/MainActivity.java
@@ -1,22 +1,23 @@
 package com.javaking.clanteam.scouting;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+
+public class MainActivity extends SherlockFragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
+		getSupportMenuInflater().inflate(R.menu.activity_main, menu);
+		return super.onCreateOptionsMenu(menu);
 	}
+
 
 }
